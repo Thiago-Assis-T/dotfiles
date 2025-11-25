480c5afe2cde704f73134978f14dd92226f11fc1
Thiago-Assis-T <thiago.assisfernandes@gmail.com>
2025-11-25 10:47:59 -0300

    many things

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -3,7 +3,6 @@ return {
 	opts = {
 		formatters_by_ft = {
 			["_"] = { "trim_whitespace" },
-			["*"] = { "codespell" },
 			lua = { "stylua", lsp_format = "fallback" },
 			php = { "php_cs_fixer", lsp_format = "fallback" },
 			bash = { "shfmt", lsp_format = "fallback" },
