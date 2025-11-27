6a0f3c715657ba1bf0cea973dd67e122695dbcef
Thiago-Assis-T <thiago.assisfernandes@gmail.com>
2025-11-26 23:28:53 -0300

    restarting the config for 0.12

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,1 +1,34 @@
 require('treesitter')
+require('lspconfig')
+
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+vim.opt.relativenumber = true
+vim.opt.number = true
+vim.opt.numberwidth = 3
+vim.opt.signcolumn = "yes"
+vim.opt.laststatus = 3
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+vim.opt.smartcase = true
+vim.opt.mouse = "a"
+vim.opt.wrap = false
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+vim.opt.ignorecase = true
+vim.opt.termguicolors = true
+vim.opt.scrolloff = 8
+vim.opt.updatetime = 50
+vim.opt.clipboard = "unnamedplus"
+vim.opt.cmdheight = 2
+vim.opt.completeopt = { "menuone", "noselect" }
+vim.opt.conceallevel = 0
+vim.opt.fileencoding = "utf-8"
+vim.spelllang = "en_ca"
+
+vim.diagnostic.config({ virtual_lines = true })
