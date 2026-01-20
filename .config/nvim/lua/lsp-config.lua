99883163c2005123b18bb9a50dbbc51d0cc7fbd3
Thiago-Assis-T <thiago.assisfernandes@gmail.com>
2026-01-20 19:54:57 -0300

    lspconfig and mason

--- .config/nvim/lua/lsp-config.lua
+++ .config/nvim/lua/lsp-config.lua
@@ -1,6 +1,8 @@
 vim.pack.add{
   { src = 'https://github.com/neovim/nvim-lspconfig' },
+  {src = 'https://github.com/mason-org/mason.nvim'},
 }
+require("mason").setup()
 vim.lsp.config('lua_ls', {
   filetypes = { 'lua' },
   root_markers = { '.luarc.json', '.git' },
