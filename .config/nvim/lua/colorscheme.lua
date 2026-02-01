e0d872f6abc1225c705120ad0b3c24ef242041f0
Thiago-Assis-T <thiago.assisfernandes@gmail.com>
2026-01-31 22:05:35 -0300

    removing the signcolumn background

--- .config/nvim/lua/colorscheme.lua
+++ .config/nvim/lua/colorscheme.lua
@@ -7,4 +7,6 @@ highlight Normal guibg=none
 highlight NonText guibg=none
 highlight Normal ctermbg=none
 highlight NonText ctermbg=none
+highlight SignColumn guibg=none
+highlight CursorLineSign guibg=none
 ]]
